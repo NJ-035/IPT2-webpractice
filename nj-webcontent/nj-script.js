67d19f25c1e01a2f49a0f8a70c69c416aed46b21
nj <kazukiuchiha574@gmail.com>
2024-09-21 16:32:01 +0800

    Adding JS for scroll reveal

--- nj-webcontent/nj-script.js
+++ nj-webcontent/nj-script.js
@@ -39,3 +39,16 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 
 };
+
+// scroll reveal
+
+ScrollReveal ({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading',{origin: 'top'});
+ScrollReveal().reveal('.home-img, .edu-exp, .hobbies-box',{origin: 'buttom'});
+ScrollReveal().reveal('.home-contact h1, .about-img',{origin: 'left'});
+ScrollReveal().reveal('.home-contact p, .about-content',{origin: 'right'});
